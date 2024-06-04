1bf33102a353c1e4fa2153e6c972568278d5d349
YoheiKajiro <te0801068@gmail.com>
2024-06-04 22:47:48 +0900

    update

--- wezterm.lua
+++ wezterm.lua
@@ -11,5 +11,45 @@ config.automatically_reload_config = false
 
 config.font_size = 15.0
 
+config.leader = { key = 'a', mods = 'CTRL', timeout_milliseconds = 2000 }
+
+config.keys = {
+  {
+    key = ',',
+    mods = 'LEADER',
+    action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+  },
+  {
+    key = '.',
+    mods = 'LEADER',
+    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+  },
+  {
+    key = "w",
+    mods = "CMD",
+    action = wezterm.action.CloseCurrentPane { confirm = true },
+  },
+  {
+    key = 'h',
+    mods = 'LEADER',
+    action = wezterm.action.ActivatePaneDirection 'Left',
+  },
+  {
+    key = 'j',
+    mods = 'LEADER',
+    action = wezterm.action.ActivatePaneDirection 'Down',
+  },
+  {
+    key = 'k',
+    mods = 'LEADER',
+    action = wezterm.action.ActivatePaneDirection 'Up',
+  },
+  {
+    key = 'l',
+    mods = 'LEADER',
+    action = wezterm.action.ActivatePaneDirection 'Right',
+  },
+}
+
 -- and finally, return the configuration to wezterm
 return config
